4c4a3c4ab2f9cae954823640c88a5c24d01bdc2c
MrSanty <sr.santyquinteror@gmail.com>
2022-04-27 22:24:17 -0500

    Iniciando Frontend

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { H1 } from "./styles/H1";
+
+
+function App() {
+  return (
+    <H1>Hello World</H1>
+  );
+}
+
+export default App;
---
+++ src/styles/H1.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #222;
+  text-align: center;
+`;
